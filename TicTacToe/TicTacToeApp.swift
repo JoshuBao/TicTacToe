27c50890e8ac3a9725aec0f8befcde8d13e0513b
Joshua Cheng <joshuacheng888@gmail.com>
2023-09-12 01:26:16 -0700

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Joshua Cheng on 9/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
